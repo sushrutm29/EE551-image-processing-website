053907c60a69ace9c75b7045848445235471c2f1
sushrutm29 <smadhavi@stevens.edu>
2021-04-11 18:09:31 -0400

    created front-end for register and login pages

--- main/routes.py
+++ main/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, url_for, flash, redirect
+from main.forms import RegistrationForm, LoginForm
 from main import app
 
 @app.route("/")
@@ -14,9 +15,20 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='Register')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Login')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
