103d322b451316b924f5f24fc4cdc100e07a3a96
sushrutm29 <smadhavi@stevens.edu>
2021-04-14 23:47:58 -0400

    register and login functionality added

--- main/__init__.py
+++ main/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_mongoengine import MongoEngine
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2ee9596db295364f62dd6aef17a2e3ca'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'chore-russian-roulette-db',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine()
+db.init_app(app)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from main import routes
